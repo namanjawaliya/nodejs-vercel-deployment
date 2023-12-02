d49072a3d864f38331a0df7a364f7d55f670ae48
Naman Jawaliya <jawaliyan@gmail.com>
2023-12-02 16:16:58 +0530

    init app

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => {
+  res.json({
+    hello: "world",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Running!!!");
+});
